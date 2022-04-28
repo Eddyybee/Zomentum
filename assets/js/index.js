594c99902a0d3ba48296b729ac633031e39e5158
amanam israel <amanam.israel@venturegardengroup.com>
2022-04-28 09:04:40 +0100

    fixed form submit => alert

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,12 @@
-const inputs = document.getElementsByTagName('input');
-inputs[0].addEventListener('change', ()=> {
-    console.log(inputs[0].value)
-})
-inputs[1].addEventListener('change', ()=> {
-    console.log(inputs[1].value)
-})
-
+const inputs = document.getElementsByTagName("input");
+inputs[0].addEventListener("change", () => {
+  console.log(inputs[0].value);
+});
+inputs[1].addEventListener("change", () => {
+  console.log(inputs[1].value);
+});
 
 function submitForm() {
-    console.log("hi")
-    // if none of the forms have an empty value. alert success.
-    if(!inputs[0].value === "" && !inputs[1].value === ""){
-        alert('you have been signed in')
-    }
+  // if none of the forms have an empty value. alert success.
+  alert(`welcome ${inputs[0].value}`);
 }
